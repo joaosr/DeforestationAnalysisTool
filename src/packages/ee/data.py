f69dd7924b4e7619012a06f257e40620c63963f2
Max Shawabkeh <maxus@google.com>
2013-05-14 11:43:35 -0700

    Update the EE library to 0.1.1.

--- src/packages/ee/data.py
+++ src/packages/ee/data.py
@@ -1,16 +1,9 @@
-# Copyright 2012 Google Inc. All Rights Reserved.
-
-"""Singleton for all of the library's communcation with the EE API.
-
-This manages the data and API communication.
-"""
+"""Singleton for all of the library's communcation with the Earth Engine API."""
 
 
 
-# Using old-style python function naming on purpose to match the
-# javascript version's naming.
-# pylint: disable-msg=C6003,C6409
-
+# Using lowercase function naming to match the JavaScript names.
+# pylint: disable-msg=g-bad-name
 
 import json
 import urllib
@@ -21,10 +14,10 @@ import ee_exception
 
 
 # The base URL for all data calls.  This is set by ee.initialize().
-BASE_URL = 'https://earthengine.googleapis.com/api'
+_api_base_url = None
 
 # The base URL for map tiles.  This is set by ee.initialize().
-TILE_BASE = 'https://earthengine.googleapis.com'
+_tile_base_url = None
 
 # The default deadline.
 DEFAULT_DEADLINE = 30
@@ -32,8 +25,17 @@ DEFAULT_DEADLINE = 30
 # OAuth2 credentials object.  This may be set by ee.Initialize().
 CREDENTIALS = None
 
-# ClientLogin authorization token.  This may be set by ee.Initialize().
-CLIENT_LOGIN_TOKEN = None
+
+def setBaseUrls(api_base_url, tile_base_url):
+  """Sets the base URLs for API calls and tiles.
+
+  Args:
+    api_base_url: The (proxied) EarthEngine REST API endpoint.
+    tile_base_url: The (unproxied) EarthEngine REST tile endpoint.
+  """
+  global _api_base_url, _tile_base_url
+  _api_base_url = api_base_url
+  _tile_base_url = tile_base_url
 
 
 def getInfo(asset_id):
@@ -61,11 +63,12 @@ def getList(asset_id):
 
 
 def getMapId(params):
-  """Get a Map Id for a given asset.
+  """Get a Map ID for a given asset.
 
   Args:
     params: An object containing visualization options with the
             following possible values:
+      image - (JSON string) The image to render.
       version - (number) Version number of image (or latest).
       bands - (comma-seprated strings) Comma-delimited list of
           band names to be mapped to RGB.
@@ -87,7 +90,29 @@ def getMapId(params):
     A dictionary containing "mapid" and "token" strings, which can
     be combined to retrieve tiles from the /map service.
   """
+  params['json_format'] = 'v2'
   return send_('/mapid', params)
+
+
+def getTileUrl(mapid, x, y, z):
+  """Generate a URL for map tiles from a Map ID and coordinates.
+
+  Args:
+    mapid: The Map ID to generate tiles for, a dictionary containing "mapid"
+        and "token" strings.
+    x: The tile x coordinate.
+    y: The tile y coordinate.
+    z: The tile zoom level.
+
+  Returns:
+    The tile URL.
+  """
+  width = 2 ** z
+  x %= width
+  if x < 0:
+    x += width
+  return '%s/map/%s/%d/%d/%d?token=%s' % (
+      _tile_base_url, mapid['mapid'], z, x, y, mapid['token'])
 
 
 def getValue(params):
@@ -100,6 +125,7 @@ def getValue(params):
   Returns:
     The value call results.
   """
+  params['json_format'] = 'v2'
   return send_('/value', params)
 
 
@@ -120,6 +146,7 @@ def getThumbnail(params):
   Returns:
     A thumbnail image as raw PNG data.
   """
+  params['json_format'] = 'v2'
   return send_('/thumb', params, opt_method='GET', opt_raw=True)
 
 
@@ -142,42 +169,43 @@ def getThumbId(params):
   """
   request = params.copy()
   request['getid'] = '1'
+  request['json_format'] = 'v2'
   return send_('/thumb', request)
 
 
 def getDownloadId(params):
-  # pylint: disable-msg=g-doc-args
-  """Get a Download Id.
+  """Get a Download ID.
 
   Args:
     params: An object containing visualization options with the following
       possible values:
-        name: a base name to use when constructing filenames.
-        bands: a description of the bands to download. Must be an array of
+        name - a base name to use when constructing filenames.
+        bands - a description of the bands to download. Must be an array of
             dictionaries, each with the following keys:
-          id: the name of the band, a string, required.
-          crs: an optional CRS string defining the band projection.
-          crs_transform: an optional array of 6 numbers specifying an affine
-              transform from the specified CRS, in the order: xScale, yShearing,
-              xShearing, yScale, xTranslation and yTranslation.
-          dimensions: an optional array of two integers defining the width and
+          id - the name of the band, a string, required.
+          crs - an optional CRS string defining the band projection.
+          crs_transform - an optional array of 6 numbers specifying an affine
+              transform from the specified CRS, in the order: xScale,
+              yShearing, xShearing, yScale, xTranslation and yTranslation.
+          dimensions - an optional array of two integers defining the width and
               height to which the band is cropped.
-          scale: an optional number, specifying the scale in meters of the band;
-                 ignored if crs and crs_transform is specified.
-        crs: a default CRS string to use for any bands that do not explicitly
+          scale - an optional number, specifying the scale in meters of the
+                 band; ignored if crs and crs_transform is specified.
+        crs - a default CRS string to use for any bands that do not explicitly
             specify one.
-        crs_transform: a default affine transform to use for any bands that do
+        crs_transform - a default affine transform to use for any bands that do
             not specify one, of the same format as the crs_transform of bands.
-        dimensions: default image cropping dimensions to use for any bands that
-            do not specify them.
-        scale: a default scale to use for any bands that do not specify one;
+        dimensions - default image cropping dimensions to use for any bands
+            that do not specify them.
+        scale - a default scale to use for any bands that do not specify one;
             ignored if crs and crs_transform is specified.
-        region: a polygon specifying a region to download; ignored if crs
+        region - a polygon specifying a region to download; ignored if crs
             and crs_transform is specified.
 
   Returns:
     A dict containing a docid and token.
   """
+  params['json_format'] = 'v2'
   return send_('/download', params)
 
 
@@ -190,8 +218,8 @@ def makeDownloadUrl(downloadId):
   Returns:
     A URL from which the download can be obtained.
   """
-  return (BASE_URL + '/download?docid=' + downloadId['docid'] +
-          '&token=' + downloadId['token'])
+  return '%s/download?docid=%s&token=%s' % (
+      _api_base_url, downloadId['docid'], downloadId['token'])
 
 
 def getAlgorithms():
@@ -223,7 +251,7 @@ def createAsset(value, opt_path=None):
   Returns:
     A description of the saved asset, including a generated ID.
   """
-  args = {'value': value}
+  args = {'value': value, 'json_format': 'v2'}
   if opt_path is not None:
     args['id'] = opt_path
   return send_('/create', args)
@@ -245,15 +273,13 @@ def send_(path, params, opt_method='POST', opt_raw=False):
   Raises:
     EEException: For malformed requests or errors from the server.
   """
-  url = BASE_URL + path
+  url = _api_base_url + path
   deadline = float(params.pop('deadline', DEFAULT_DEADLINE))
   payload = urllib.urlencode(params)
   http = httplib2.Http(timeout=deadline)
 
   headers = {}
-  if CLIENT_LOGIN_TOKEN:
-    headers['Authorization'] = 'GoogleLogin auth=' + CLIENT_LOGIN_TOKEN
-  elif CREDENTIALS:
+  if CREDENTIALS:
     http = CREDENTIALS.authorize(http)
 
   if opt_method == 'GET':
@@ -280,6 +306,6 @@ def send_(path, params, opt_method='POST', opt_raw=False):
     content = json.loads(content)
     if 'error' in content:
       raise ee_exception.EEException(content['error'])
-    if not 'data' in content:
+    if 'data' not in content:
       raise ee_exception.EEException('Missing data in response: ' + content)
     return content['data']
